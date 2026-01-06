94da3530876786f19e9100c6f6ae18cb79d1e40b
AI-Laptop <kpmgailaptop@gmail.com>
2026-01-06 17:14:31 +0530

    Update settings.py
    Change 5

--- config/settings.py
+++ config/settings.py
@@ -98,7 +98,7 @@ MIGRATION_MODULES = {"sites": "matorral.contrib.sites.migrations"}
 # DEBUG
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = env.bool("DJANGO_DEBUG", False)
+DEBUG = True
 
 # FIXTURE CONFIGURATION
 # ------------------------------------------------------------------------------
